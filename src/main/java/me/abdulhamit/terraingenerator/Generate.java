acee9f94a0f5fae85fdc18d5f772cb2282c344f6
hamit <hamitefe0161@gmail.com>
2023-08-11 20:28:07 +0300

    first upload

--- src/main/java/me/abdulhamit/terraingenerator/Generate.java
+++ src/main/java/me/abdulhamit/terraingenerator/Generate.java
@@ -3,7 +3,6 @@ package me.abdulhamit.terraingenerator;
 import de.articdive.jnoise.generators.noise_parameters.fade_functions.FadeFunction;
 import de.articdive.jnoise.generators.noise_parameters.interpolation.Interpolation;
 import de.articdive.jnoise.pipeline.JNoise;
-import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -12,10 +11,9 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.util.noise.NoiseGenerator;
-import org.bukkit.util.noise.PerlinNoiseGenerator;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Random;
 
 public class Generate implements CommandExecutor {
@@ -31,9 +29,10 @@ public class Generate implements CommandExecutor {
         try {
             double smoothness = Double.parseDouble(args[0]);
             double height = Double.parseDouble(args[1]);
-            int size = Integer.parseInt(args[2]);
+            int waterHeight = Integer.parseInt(args[2]);
+            int size = Integer.parseInt(args[3]);
             
-            generateChunk((Player) commandSender, smoothness, height, size);
+            generateTerrain((Player) commandSender, smoothness, height,waterHeight, size);
         } catch (Exception e){
             commandSender.sendMessage("Error executing command: "+e.getMessage());
         }
@@ -44,18 +43,36 @@ public class Generate implements CommandExecutor {
     }
     
     
-    public static void generateChunk(Player p, double smoothness, double yscale, int size){
+    public static void generateTerrain(Player p, double smoothness, double yscale, int waterheight, int size){
         final Location loc = p.getLocation().clone();
         final World world = loc.getWorld();
-        
+        long startTick = System.currentTimeMillis();
         for (int x = -size; x<size; x++){
             for (int z = -size; z<size; z++){
                 int height = (int) Math.round(noisePipeline.evaluateNoise((loc.getX()+x)/smoothness, (loc.getZ()+z)/smoothness)*yscale);
-                for (int y = height; y >=-20; y--){
-                    new Location(world,loc.getX()+x, y, loc.getZ()+z).getBlock().setType(Material.STONE);
+                int grassHeight = height;
+                int dirtHeight = grassHeight-3;
+                for (int y = (height<waterheight) ? waterheight : height; y >=-20; y--){
+                    Block block = new Location(world,loc.getX()+x, y, loc.getZ()+z).getBlock();
+                    if (y<=height) {
+                        if (y == grassHeight&&y>waterheight-1) {
+                            block.setType(Material.GRASS_BLOCK);
+                            placeFlower(block.getRelative(0, 1, 0));
+                        } else if (y >= dirtHeight&&y>waterheight-1) {
+                            block.setType(Material.DIRT);
+                        }else {
+                            block.setType(Material.STONE);
+                        }
+                    } else if(y<=waterheight) {
+                        block.setType(Material.WATER);
+                    } else {
+                        block.setType(Material.AIR);
+                    }
                 }
             }
         }
+        
+        p.sendMessage("generated in "+(System.currentTimeMillis()-startTick)+" milli seconds");
     }
     
     public static void resetSeed(){
@@ -65,5 +82,13 @@ public class Generate implements CommandExecutor {
                 .scale(40.0)
                 .perlin(seed, Interpolation.COSINE, FadeFunction.SMOOTHSTEP)
                 .build();
+    }
+    
+    private static void placeFlower(Block b){
+        List<Material> blocks = List.of(
+                Material.GRASS,
+                Material.AIR
+        );
+        b.setType(blocks.get(new Random().nextInt(blocks.size())));
     }
 }
